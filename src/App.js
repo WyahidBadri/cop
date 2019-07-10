16e5dfdd8e88b38415eb7eed0cbd24fe9861e688
Wyahid Badri <wyahidb2015@gmail.com>
2019-07-10 12:07:33 -0400

    added hooks

--- src/App.js
+++ src/App.js
@@ -1,59 +1,30 @@
-import React, {Component, useState} from 'react';
-import './App.css';
+import React, { useState, useEffect} from 'react';
 import SubTotal from './components/Subtotal/Subtotal';
 import TaxFees from './components/TaxFees/TaxFees'
 import EstimatedTotal from './components/EstimatedTotal/EstimatedTotal';
 import ShippingPrice from './components/ShippingPrice/ShippingPrice';
 import ItemDetail from './components/ItemDetail/ItemDetail'
+import './App.css';
 
-class  App extends Component {
-  constructor(props){
-    super(props);
-
-    this.state = {
-      total: 100,
-      taxes: 0,
-      estimatedTotal: 0,
-      shipping: 0
-    }
-
-    
-  }
+export default function App(){
   
-    // const [total, setTotal] = useState();
-    // const [taxes, SetTaxes] = useState();
-    // const [estimatedTotal, setEstimatedTotal] = useState();
-    // const [shipping, setShipping] = useState();
-  
- 
+    const [total, setTotal] = useState(0);
+    useEffect(()=> {
+     setTotal(100)
+    },[]
+    ) 
 
-  componentDidMount = () => {
-     this.setState({
-       taxes: (this.state.total) * 0.0875
-     },
-     function(){
-       this.setState({
-         shipping: this.state.total / 20
-       })
-       this.setState({
-        estimatedTotal: this.state.total + this.state.taxes + this.state.shipping
-      })
-     },
-     )
-  }
-  render(){
+    let taxes = total * 0.0875;
+    let shipping = total / 20;
+    let estimatedTotal = total + taxes + shipping;
+       
     return (
       <div className="container">
-
-      <SubTotal price={this.state.total.toFixed(2)} />
-      <TaxFees taxes={this.state.taxes.toFixed(2)} />
-      <ShippingPrice ship={this.state.shipping.toFixed(2)}/>
-      <EstimatedTotal price={this.state.estimatedTotal.toFixed(2)}/>
-      <ItemDetail price={this.state.estimatedTotal.toFixed(2)}/>
-      
+      <SubTotal price={total} />
+      <TaxFees taxes={taxes} />
+      <ShippingPrice ship={shipping}/>
+      <EstimatedTotal price={estimatedTotal}/>
+      <ItemDetail price={estimatedTotal}/>     
       </div>
     )
-  }
 }
-
-export default App;
